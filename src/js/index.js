b25f853c972cc8c173fffe9aa161a2424fb1377f
Andrea <lessricc@gmail.com>
2024-06-06 21:59:18 +0200

    vamos mejorando

--- src/js/index.js
+++ src/js/index.js
@@ -4,17 +4,27 @@ import '../scss/styles.scss';
 const campoEntrada = document.getElementById('entrada');
 const campoSalida = document.getElementById('salida');
 
-campoEntrada.addEventListener('keyup', function (event) {
-  console.log(event);
-  const valoresAceptados = /^[0-9]+$/;
-  if (!campoEntrada.value.match(valoresAceptados)) {
-    campoEntrada.value = '';
-  }
-});
+// campoEntrada.addEventListener('keyup', function (event) {
+//   console.log(event);
+//   const valoresAceptados = /^[0-9]+$/;
+//   if (!campoEntrada.value.match(valoresAceptados)) {
+//     campoEntrada.value = '';
+//   }
+// });
 
-//Elabore un programa con javascript que en el navegador permita a través de un input en el que se introduce un número y del click de un botón actualizar un contador en pantalla que reduzca en 1 unidad con cada click el número introducido.
+//Elabore un programa con javascript que en el navegador permita a través la lectura de un número x que se escriba en el html y el click de un botón que permita generar la tabla de multiplicar de ese número (x del 1 al 10)
 
 const totalNumber = document.getElementById('number');
+const clickButton = document.getElementById('button');
 
-const 
-
+clickButton.addEventListener('click', function (evt) {
+  console.log(evt);
+  const totalNumberValue = totalNumber.value;
+  for (let i = 1; i <= 10; i++) {
+    const parrafo = document.createElement('p');
+    const multiplicacion = totalNumberValue * i;
+    const texto = document.createTextNode(`${i} x ${totalNumberValue} = ${multiplicacion}`);
+    parrafo.appendChild(texto);
+    document.body.appendChild(parrafo);
+  }
+});

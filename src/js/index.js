6a5ec0913e67103924af188cc3c05400ac86fea9
Andrea <lessricc@gmail.com>
2024-06-10 21:53:16 +0200

    me falta uno

--- src/js/index.js
+++ src/js/index.js
@@ -31,16 +31,16 @@ const campoSalida = document.getElementById('salida');
 
 //Elabore un programa con javascript que en el navegador permita a través de un input en el que se hace una entrada de una frase de texto y el click de un botón que permita contar todas las vocales de la frase escrita, imprima una lista con cada vocal y la cantidad de veces que fue encontrada en la frase, no tenga en cuenta distinción entre mayúsculas y minúsculas.
 
-const inputText = document.getElementById('textInput').value.toLowerCase();
+const inputText = document.getElementById('textInput');
 const button = document.getElementById('button');
 
-function vowelscounter() {
+button.addEventListener('click', function (evento) {
   let countA = 0;
   let countE = 0;
   let countI = 0;
   let countO = 0;
   let countU = 0;
-  for (let vowel of inputText) {
+  for (let vowel of inputText.value.toLowerCase()) {
     if (vowel === 'a') {
       countA++;
     } else if (vowel === 'e') {
@@ -53,5 +53,8 @@ function vowelscounter() {
       countU++;
     }
   }
-  console.log(vowelscounter);
-}
+  console.log(`A: ${countA}, E: ${countE}, I: ${countI}, O: ${countO}, U: ${countU}`);
+});
+
+
+//Elabore un programa con javascript que en el navegador permita a través de un input en el que se hace una entrada de teclado detectar si es un número o una letra, si es letra determine si es consonante o una vocal y la borre el carácter del input en cualquier caso, luego un botón que permite ver la lista de letras que han sido introducidas anteriormente y la cantidad de veces que fueron introducidas, no tenga en cuenta distinción entre mayúsculas y minúsculas.

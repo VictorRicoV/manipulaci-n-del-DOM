bc38899db91f80e7c5360d14bbf55310d8dcf9a4
Andrea <lessricc@gmail.com>
2024-06-11 21:59:33 +0200

    terminamos los ejercicios

--- src/js/index.js
+++ src/js/index.js
@@ -31,30 +31,37 @@ const campoSalida = document.getElementById('salida');
 
 //Elabore un programa con javascript que en el navegador permita a través de un input en el que se hace una entrada de una frase de texto y el click de un botón que permita contar todas las vocales de la frase escrita, imprima una lista con cada vocal y la cantidad de veces que fue encontrada en la frase, no tenga en cuenta distinción entre mayúsculas y minúsculas.
 
+
+// button.addEventListener('click', function (evento) {
+  //   let countA = 0;
+  //   let countE = 0;
+  //   let countI = 0;
+  //   let countO = 0;
+  //   let countU = 0;
+  //   for (let vowel of inputText.value.toLowerCase()) {
+    //     if (vowel === 'a') {
+      //       countA++;
+      //     } else if (vowel === 'e') {
+        //       countE++;
+        //     } else if (vowel === 'i') {
+          //       countI++;
+          //     } else if (vowel === 'o') {
+            //       countO++;
+            //     } else if (vowel === 'u') {
+              //       countU++;
+              //     }
+              //   }
+              //   console.log(`A: ${countA}, E: ${countE}, I: ${countI}, O: ${countO}, U: ${countU}`);
+              // });
+              
+              
+//Elabore un programa con javascript que en el navegador permita a través de un input en el que se hace una entrada de teclado detectar si es un número o una letra, si es letra determine si es consonante o una vocal y la borre el carácter del input en cualquier caso, luego un botón que permite ver la lista de letras que han sido introducidas anteriormente y la cantidad de veces que fueron introducidas, no tenga en cuenta distinción entre mayúsculas y minúsculas.
+              
+              
+              
 const inputText = document.getElementById('textInput');
 const button = document.getElementById('button');
 
-button.addEventListener('click', function (evento) {
-  let countA = 0;
-  let countE = 0;
-  let countI = 0;
-  let countO = 0;
-  let countU = 0;
-  for (let vowel of inputText.value.toLowerCase()) {
-    if (vowel === 'a') {
-      countA++;
-    } else if (vowel === 'e') {
-      countE++;
-    } else if (vowel === 'i') {
-      countI++;
-    } else if (vowel === 'o') {
-      countO++;
-    } else if (vowel === 'u') {
-      countU++;
-    }
-  }
-  console.log(`A: ${countA}, E: ${countE}, I: ${countI}, O: ${countO}, U: ${countU}`);
-});
-
-
-//Elabore un programa con javascript que en el navegador permita a través de un input en el que se hace una entrada de teclado detectar si es un número o una letra, si es letra determine si es consonante o una vocal y la borre el carácter del input en cualquier caso, luego un botón que permite ver la lista de letras que han sido introducidas anteriormente y la cantidad de veces que fueron introducidas, no tenga en cuenta distinción entre mayúsculas y minúsculas.
+function dectetorDeLetras (){
+  
+}
